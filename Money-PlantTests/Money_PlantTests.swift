3e46541c1ebf78e80a682414647aa6d3f9d976a7
Rohan Soni <rohansoni342ni@gmail.com>
2024-10-23 13:43:50 +0530

    storyboard

---
+++ Money-PlantTests/Money_PlantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Money_PlantTests.swift
+//  Money-PlantTests
+//
+//  Created by Rohan on 21/10/24.
+//
+
+import Testing
+@testable import Money_Plant
+
+struct Money_PlantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
